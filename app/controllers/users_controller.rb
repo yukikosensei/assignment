3ed06a6e650e036e6d44f69ee2dfc78118db0437
yukikosensei <wwwdtad@gmail.com>
2021-06-04 17:38:04 -0400

    Able to make a account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,11 @@
 class UsersController < ApplicationController
-  # index, show, edit, new, create, update, destroy
-
-  # signup page
   def new
     @user = User.new
   end
 
-  # form submit (create user, send email)
   def create
-    email = params[:user][:email]
-    name = params[:user][:name]
-    company_name = params[:user][:company_name]
-    phone_number1 = params[:user][:phone_number1]
-    phone_number2 = params[:user][:phone_number2]
-    phone_number3 = params[:user][:phone_number3]
-    password = params[:user][:password]
-    password_confirmation = params[:user][:password_confirmation]
-    UserNotifierMailer.send_contact_email(email, name, company_name, phone_number1, phone_number2, phone_number3, password, password_confirmation).deliver_now
+    UserNotifierMailer.send_contact_email(user_params).deliver_now
+    User.create(user_params)
     flash[:success] = "登録申請が完了しました。"
     redirect_to new_user_path
   end
--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -2,15 +2,13 @@ class UserNotifierMailer < ActionMailer::Base
   default :from => 'nihongoyukikosensei@gmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
-  def send_contact_email(email, name, company_name, phone_number1, phone_number2, phone_number3, password, password_confirmation)
-    @email = email
-    @name = name
-    @company_name = company_name
-    @phone_number1 = phone_number1
-    @phone_number2 = phone_number2
-    @phone_number3 = phone_number3
-    @password = password
-    @password_confirmation = password_confirmation
+  def send_contact_email(user_params)
+    @email = user_params[:email]
+    @name = user_params[:name]
+    @company_name = user_params[:company_name]
+    @phone_number1 = user_params[:phone_number1]
+    @phone_number2 = user_params[:phone_number2]
+    @phone_number3 = user_params[:phone_number3]
     mail( :to => "nihongoyukikosensei@gmail.com",
     :subject => "会員登録申請")
   end
